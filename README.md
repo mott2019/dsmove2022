7de36c93e24dd66d465d4e6b62c723e26724d736
André Mott <vendasobambu@gmail.com>
2022-01-16 22:30:31 -0300

    Project created

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dsmove2022
+#echo "# dsmove2022" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/mott2019/dsmove2022.git
+#git push -u origin main
---
+++ backend/src/test/java/com/mott2019/dsmove/DsmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mott2019.dsmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
